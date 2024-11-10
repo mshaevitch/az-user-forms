0d22958cef52d37fc8e2a30aeef5e1de95a018a3
Michael Shaevitch <michael.shaevitch@gmail.com>
2024-11-10 17:13:21 -0500

    feat: create form using shadcn/ui and zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,44 @@
 'use client'
 import { useState } from 'react'
-import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from '@/components/ui/mode-toggle'
- 
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  msg: z.string().min(2, {
+    message: "Message must be at least 2 characters.",
+  }),
+})
+
 export default function Home() {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
   const [response, setResponse] = useState('');
- 
-  const handleSubmit = async () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      msg: "",
+    },
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const name = values.username
+    const message = values.msg
     try {
       const res = await fetch('/api/hello', {
         method: 'POST',
@@ -28,17 +57,54 @@ export default function Home() {
   }
  
   return (
-    <main className="min-h-screen relative p-6">
+    <main>
       <div className="absolute top-6 right-6">
         <ModeToggle />
       </div>
-      <div className="flex min-h-screen items-center justify-center">
-        <div className="w-full max-w-md space-y-4">
-          {response && <div className="text-center">{response}</div>}
-          <Textarea placeholder="Type your name here." value={name} onChange={(e) => setName(e.target.value)} />
-          <Textarea placeholder="Type your message here." value={message} onChange={(e) => setMessage(e.target.value)} />
-          <Button className="w-full" onClick={handleSubmit}>Send message</Button>
+      {response && (
+        <div className="fixed top-32 left-1/2 -translate-x-1/2 w-full max-w-md p-4 bg-gray-800 rounded border border-gray-700">
+          <h2 className="font-medium mb-2">Response:</h2>
+          <p>{response}</p>
         </div>
+      )}
+      <div className="flex min-h-screen items-center justify-center">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your name" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Please enter your name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="msg"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter a message" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Please enter a message.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     </main>
   )
