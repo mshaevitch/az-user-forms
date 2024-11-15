fdd727c32b17b3afb20e4ef4afa1c6d115f253c8
Michael Shaevitch <michael.shaevitch@gmail.com>
2024-11-14 22:00:36 -0500

    Add log out button (in progress)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
 'use client'
 import { ModeToggle } from '@/components/ui/mode-toggle'
 import { NewUserForm } from '@/components/forms/new-user-form'
+import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch('/.auth/me');
+          if (!response.ok) {
+            throw new Error('Failed to fetch user info');
+          }
+        const data = await response.json();
+        setUser(data.clientPrincipal.userDetails);
+      } catch (err) {
+        console.error("Error fetching user info:", err);
+      }
+    }
+    fetchUser();
+  }, []);
+
   return (
     <main>
       <div className="absolute top-6 right-6">
         <ModeToggle />
+        <Button>{user}</Button>
       </div>
       {/* {response && (
         <div className="fixed top-32 left-1/2 -translate-x-1/2 w-full max-w-md p-4 bg-gray-800 rounded border border-gray-700">
